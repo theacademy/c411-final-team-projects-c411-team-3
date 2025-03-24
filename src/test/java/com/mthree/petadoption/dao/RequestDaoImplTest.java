16d3757760862c1f3d86094c4f6a1782a73b6675
Nikkita Tarrao <nikkita.tarrao19@my.stjohns.edu>
2025-03-24 16:38:06 -0400

    created the test class for RequestDaoImpl

---
+++ src/test/java/com/mthree/petadoption/dao/RequestDaoImplTest.java
@@ -0,0 +1,28 @@
+package com.mthree.petadoption.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestDaoImplTest {
+
+    @Test
+    void listAllRequests() {
+    }
+
+    @Test
+    void viewRequest() {
+    }
+
+    @Test
+    void submitRequest() {
+    }
+
+    @Test
+    void updateRequest() {
+    }
+
+    @Test
+    void cancelRequest() {
+    }
+}
