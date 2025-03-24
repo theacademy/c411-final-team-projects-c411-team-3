b2fdfd2ea4b589ca0b00c210a5985ad0e17fc74e
Kris Su <cysu1205@gmail.com>
2025-03-24 14:41:31 -0400

    complete basic crud of pet

--- src/main/java/com/mthree/petadoption/dao/PetDAOImpl.java
+++ src/main/java/com/mthree/petadoption/dao/PetDAOImpl.java
@@ -1,6 +1,7 @@
 package com.mthree.petadoption.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -8,14 +9,39 @@ import com.mthree.petadoption.model.Pet;
 import com.mthree.petadoption.repository.PetRepository;
 
 @Repository
-public class PetDAOImpl implements PetDAO{
+public class PetDAOImpl implements PetDAO {
   private final PetRepository petRepository;
 
   public PetDAOImpl(PetRepository petRepository) {
     this.petRepository = petRepository;
   }
+
   @Override
   public List<Pet> findAllPets() {
     return petRepository.findAll();
+  }
+
+  @Override
+  public Optional<Pet> findPetById(Long id) {
+    return petRepository.findById(id);
+  }
+
+  @Override
+  public Pet savePet(Pet pet) {
+    return petRepository.save(pet);
+  }
+
+  @Override
+  public boolean deletePet(Long id) {
+    if (petRepository.existsById(id)) {
+      petRepository.deleteById(id);
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public Pet updatePet(Pet pet) {
+    return petRepository.save(pet);
   }
 }
