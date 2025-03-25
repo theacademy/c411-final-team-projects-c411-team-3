dd9a62a2a5a6df81caf702959e84f43d3b21b70f
Kris Su <cysu1205@gmail.com>
2025-03-25 15:11:10 -0400

    update-pet-service

--- src/main/java/com/mthree/petadoption/repository/PetRepository.java
+++ src/main/java/com/mthree/petadoption/repository/PetRepository.java
@@ -3,5 +3,8 @@ package com.mthree.petadoption.repository;
 import com.mthree.petadoption.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
+  List<Pet> findBySpecies(String species);
 }
