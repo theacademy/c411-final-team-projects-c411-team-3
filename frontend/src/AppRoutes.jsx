3866cd2047bfd3ddb7973bac3baee1c2e2a1c48b
Nikkita Tarrao <nikkita.tarrao19@my.stjohns.edu>
2025-03-27 16:13:21 -0400

    I got rid of the submit-request frontend

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProfileSetup from "./pages/ProfileSetup";
-import SubmitRequest from "./pages/SubmitRequest";
 import ViewRequest from "./pages/ViewRequest";
 
 const AppRoutes = () => {
@@ -12,7 +11,6 @@ const AppRoutes = () => {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile-setup" element={<ProfileSetup />} />
-            <Route path="/submit-request" element={<SubmitRequest />} />
             <Route path="/view-request" element={<ViewRequest />} />
         </Routes>
       </Router>
