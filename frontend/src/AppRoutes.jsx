da39ae5bbb4822570e9b27fcde82bec782dac8df
Kris Su <cysu1205@gmail.com>
2025-03-26 22:15:44 -0400

    beautify the web pages of pets

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProfileSetup from "./pages/ProfileSetup";
+import AllPetsPage from "./pages/AllPetsPage";
+import PetDetailPage from "./pages/PetDetailPage";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +12,8 @@ const AppRoutes = () => {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile-setup" element={<ProfileSetup />} />
+          <Route path="/pets" element={<AllPetsPage />} />
+          <Route path="/pets/:id" element={<PetDetailPage />} />
         </Routes>
       </Router>
   );
